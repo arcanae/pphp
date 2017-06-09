5919759981ad43b29ca7320c0b2dcf7b512d3a73
Arcanae <justin.courdesse@gmail.com>
2017-06-09 17:31:09 +0200

    \n

--- blogPHP/create-file.php
+++ blogPHP/create-file.php
@@ -12,7 +12,7 @@ else {
 
 $open = fopen("BlogFolder/".$title.".txt", "a");
 $open;
-fwrite($open,$title."\n \n".$text."\n"."~~ by ".$user."\n");
+fwrite($open,$title."\n \n".$text."\n"."~~ by ".$user."\n \n");
 fclose($open);
 
 // echo "You've create ".$title.".txt with the text :</br></br>".$text;
